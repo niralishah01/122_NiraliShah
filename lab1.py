782c8ba10210e7f4355222275df13edf4e46832a
Nirali Shah <niralipshah2000@gmail.com>
2021-07-09 16:54:48 +0530

    initial commit for lab1

--- lab1.py
+++ lab1.py
@@ -1,5 +1,10 @@
 import numpy as np
 import pandas as pd
+import nltk
+from nltk.corups import twitter_samples
+import matplotlib.pyplot as plt
+import random 
 #numpy is used for array
 #matplotlib is used for visualization
 #nlkt
+nltk.download('twitter_samples')
